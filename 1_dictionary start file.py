ce026ee6ffbad44305df4b09c47eb3aa0afcad2f
Logan-Bowen23 <logan_bowen1@baylor.edu>
2023-01-30 15:44:11 -0600

    Check up on the second .py

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,16 +1,25 @@
 import random
+#
+phonebook = {}
+phonebook = {'Chris': '555−1111',
+             'Katie': '555−2222',
+             'Joanne': '555−3333'}
+"""
 
-phonebook = {'Chris':'555−1111',
-             'Katie':'555−2222',
-             'Joanne':'555−3333'}
-
-
+# Key is always a string value on the left, the value part is on the right and can be anything
+# Object-oriented language, attributes that described the objects and methods that carry out features on objects
+# API lets you operate websites behind the scenes -json file is returned after authenticated
+# The json file is a list of dictionaries, and is very long (this is essentially scrapping for information)
 
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+mydictionary = dict(m=8, n=9)
+print(mydictionary)
 
+print(len(phonebook))
+print(type(phonebook))
 
 
 
@@ -19,7 +28,7 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
@@ -27,9 +36,14 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+# phone = phonebook["chris"]
+# print(phone)
+name = "chris"
 
-
-
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(f"{name} is not in the phonebook")
 
 
 print()
@@ -37,69 +51,67 @@ print('*****  end section 2 ********')
 print()
 
 
-
-
-
-
-
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook['Chris'] = '555,0123'
+phonebook['Joe'] = '555-4444'
 
-
+print(phonebook)
 
 print()
 print('*****  end section 3 ********')
 print()
 
 
-
-
-
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+print(phonebook)
 
+del phonebook(['Chris'])
 
+print(phonebook)
 
 print()
 print('*****  end section 4 ********')
 print()
-
-
-
-
-
-
+"""
+"""
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
+for key in phonebook:
+    print(f"the key is {key} and the value is {phonebook[key]}")
+for value in phonebook.values():
+    print(value)
 
+for key, value in phonebook.items():
+    print(f'the key value is {key} and the value is {value}')
 
+for ph_tuple in phonebook.items():
+    print(ph_tuple)
 
-
-
+# Immutible as a tuple
 
 print()
 print('*****  end section 5 ********')
 print()
-
-
-
-
-
+"""
+"""
 print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+phone = phonebook.get('Chris', '999')
+print(phone)
 
-
-
-
+phonebook.clear()
+print(phonebook)
 
 print()
 print('*****  end section 6 ********')
@@ -111,48 +123,44 @@ print()
 print('*****  start section 7 - using pop method ********')
 print()
 
-
-
-
-
+remove = phonebook.pop('Chris', 'not found')
+print(remove)
+print(phonebook)
 
 print()
 print('*****  end section 7 ********')
 print()
 
 
-
 print()
 print('*****  start section 8 - using popitem ********')
 print()
 
+a = phonebook.popitem()
 
+print(a)
+print(phonebook)
 
-
-
+# There is no direct random pop for dictionaries
 
 print()
 print('*****  end section 8 ********')
 print()
-
-
+"""
 
 print()
 print('*****  start section 9 - using random and converting to list ********')
 print()
 
+# phonebook_list = list(phonebook)
+# print(phonebook_list)
+# random_key = random.choice(phonebook_list)
+# print(random_key)
+# print(phonebook[random_key])
+# Now turning this into one line of code, makes it faster and simpler
 
-
-
+print(phonebook[random.choice(list(phonebook))])
 
 print()
 print('*****  end section 9 ********')
 print()
-
-
-'''
-
-
-
-
-
